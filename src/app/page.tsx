a7d49c3404a089804412fd6d4c53a91cecce4bfc
Kartikay Singh <kartikay.singh.0211@gmail.com>
2025-03-26 22:14:19 +0530

    Completed services section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@
 import { HeroSection } from "@/app/Hero section/page";
 import { AboutSection } from "./About Section/page";
 import { Navbar } from "../components/navbar";
+import { ServicesSection } from "./Services Section/page";
 
 export default function Home() {
 	return (
 		<>
 			<Navbar /> <HeroSection />
 			<AboutSection />
+			<ServicesSection />
 		</>
 	);
 }
---
+++ src/components/ServiceCards.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { Carousel } from "./ui/carousel";
+export function ServiceCards() {
+	const slideData = [
+		{
+			title: "FFCS Planner",
+			button: "Check it out",
+			site: "https://ffcs.vitrendz.com/",
+			src: "https://images.unsplash.com/photo-1494806812796-244fe51b774d?q=80&w=3534&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+		},
+		{
+			title: "CGPA Calculator",
+			button: "Check it out",
+			site: "https://calculator.vitrendz.com/",
+			src: "https://images.unsplash.com/photo-1518710843675-2540dd79065c?q=80&w=3387&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+		},
+		{
+			title: "Hostel Review",
+			button: "Check it out",
+			site: "https://hostel.vitrendz.com/",
+			src: "https://images.unsplash.com/photo-1590041794748-2d8eb73a571c?q=80&w=3456&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+		},
+		{
+			title: "Roommate Finder",
+			button: "Check it out",
+			site: "https://roommate.vitrendz.com/",
+			src: "https://images.unsplash.com/photo-1679420437432-80cfbf88986c?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+		},
+	];
+	return (
+		<div className="relative overflow-hidden w-full h-full py-20">
+			<Carousel slides={slideData} />
+		</div>
+	);
+}
