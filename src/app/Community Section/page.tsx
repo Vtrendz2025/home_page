aeccb9b5ac1b744125e9618a3b3f4dff54fdd14d
Kartikay Singh <kartikay.singh.0211@gmail.com>
2025-04-05 13:38:29 +0530

    New community section

--- src/app/Community Section/page.tsx
+++ src/app/Community Section/page.tsx
@@ -1,18 +1,15 @@
-import { CommunityTabs } from "@/components/CommunityTabs";
-// import { AuroraBackground } from "@/components/ui/aurora-background";
+// import { CommunityTabs } from "@/components/CommunityTabs";
+
+import { CommunityCards } from "@/components/CommunityCards";
 
 export const CommunitySection = () => {
 	return (
 		<div id="CommunitySection" className="h-fit p-10 pt-28 pb-20 w-full">
-			{/* <AuroraBackground
-				className="h-fit p-10 pt-28 pb-20 w-full"
-				
-			> */}
-			<h1 className="text-center text-5xl text-white bg-black font-bold -mb-20">
+			<h1 className="text-center text-5xl text-white bg-black font-bold">
 				Our Community
 			</h1>
-			<CommunityTabs />
-			{/* </AuroraBackground> */}
+			{/* <CommunityTabs /> */}
+			<CommunityCards />
 		</div>
 	);
 };
